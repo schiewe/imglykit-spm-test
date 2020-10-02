3fad0e9071bf65040ded30c99c985629bee3cb72
Sascha Schwabbauer <sascha.schwabbauer@img.ly>
2020-10-02 12:28:39 +0200

    Update

---
+++ Package.swift
@@ -0,0 +1,16 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+  name: "ImglyKit",
+  platforms: [.iOS(.v9)],
+  products: [
+    .library(name: "ImglyKit", targets: ["ImglyKit"]),
+  ],
+  targets: [
+    .binaryTarget(
+        name: "ImglyKit", 
+        url: "https://www.dropbox.com/s/fyps5h9ntvactja/ImglyKit.zip?dl=1", 
+        checksum: "4e580cf3109e3c55e34259baa84d06257eee0906b16f48376e32afa80aeb238a"),
+  ]
+)
