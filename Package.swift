76ab51de34938aa556da23c9b69a23a59e8a5849
Alexander Schiewe <alexander.schiewe@img.ly>
2021-01-20 13:47:42 +0100

    Test 10.21.3

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
   targets: [
     .binaryTarget(
         name: "ImglyKit", 
-        url: "https://github.com/schiewe/imglykit-spm-test/releases/download/10.21.2/ImglyKit-10.21.2.zip", 
-        checksum: "494c77c02c06be0f23a3dfdb6deeca522c89e82fdc9fea9d6d98d007883fcac7"),
+        url: "https://github.com/schiewe/imglykit-spm-test/releases/download/10.21.3/ImglyKit-10.21.3.zip", 
+        checksum: "285fa98b7dbbf296035aa248f65ed4e09f519257641de74bf70317a5e4fb0f6c"),
   ]
 )
