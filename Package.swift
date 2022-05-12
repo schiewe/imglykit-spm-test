52ef398cbd757de742475b72aebb462547030c75
Alexander Schiewe <alexander.schiewe@img.ly>
2022-05-12 15:20:28 +0200

    Add version 11.2.0-beta.2

--- Package.swift
+++ Package.swift
@@ -3,14 +3,18 @@ import PackageDescription
 
 let package = Package(
   name: "ImglyKit",
-  platforms: [.iOS(.v13)],
+  platforms: [
+    .iOS(.v13),
+    .macCatalyst(.v13),
+    .macOS(.v10_15),
+  ],
   products: [
     .library(name: "ImglyKit", targets: ["ImglyKit"]),
   ],
   targets: [
     .binaryTarget(
         name: "ImglyKit", 
-        url: "https://releases.photoeditorsdk.com/ios/ImglyKit-11.2.0-beta.1.xcframework.zip", 
-        checksum: "a40c5cbc3b61b3ab384ad8ce2b64d14d02c7ad6ee2290514ef18e9974a663e1e"),
+        url: "https://releases.photoeditorsdk.com/ios/ImglyKit-11.2.0-beta.2.xcframework.zip", 
+        checksum: "f19b453ee54be83a80ba1c2187b106ac7716dc84f1d938e8cac2bcaf7e00d0bd"),
   ]
 )
